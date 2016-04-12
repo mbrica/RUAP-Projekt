00ca4865647e0f4fa24556a37224e91c7afe13ad
Your Name <you@example.com>
2016-04-12 13:50:39 +0200

    Poruka 2

---
+++ ruap-lv1/ruap-lv1/ruap-lv1.cpp
@@ -0,0 +1,13 @@
+// ruap-lv1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello World! DR234");
+	return 0;
+}
+
